20ba2679fce3be065c3b0c133d0eb58f31fbcb0b
joestack <joern@joern-online.de>
2023-01-12 11:17:25 +0100

    first commit

---
+++ output.tf
@@ -0,0 +1,28 @@
+output "Bastionhost_public_IP" {
+  value = "ssh ${var.ssh_user}@${aws_instance.bastionhost.public_ip}"
+}
+
+output "Bastionhost_DNS" {
+  value = aws_route53_record.bastionhost.name
+}
+
+output "ELB_public_DNS" {
+  value = "${aws_route53_record.elb.name}.${var.dns_domain}"
+}
+
+# output "inventory" {
+#   value = data.template_file.ansible_skeleton.rendered
+# }
+
+# output "ansible_hosts" {
+#   value = data.template_file.ansible_web_hosts.*.rendered
+# }
+
+output "web_node_ips" {
+  value = aws_instance.web_nodes.*.private_ip
+}
+
+# output "ELB_AWS_internal_DNS" {
+#   value = aws_elb.web-elb.dns_name
+# }
+
---
+++ variables.tf
@@ -0,0 +1,48 @@
+variable "aws_region" {
+  description = "AWS region"
+  default     = "eu-west-1"
+}
+
+variable "name" {
+  description = "Unique name of the deployment"
+}
+
+variable "web_node_count" {
+  description = "number of worker nodes"
+  default     = "5"
+}
+
+variable "instance_type" {
+  description = "instance size to be used for worker nodes"
+  default     = "t2.small"
+}
+
+variable "web_subnet_count" {
+  description = "number of subnets to be used for working nodes"
+  default     = "2"
+}
+
+variable "ssh_user" {
+  description = "default ssh user to get access to an instance"
+  default     = "ubuntu"
+}
+
+variable "pub_key" {
+  description = "the public key to be used to access the bastion host and ansible nodes"
+  default     = "joestack"
+}
+
+variable "pri_key" {
+  description = "the base64 encoded private key to be used to access the bastion host and ansible nodes"
+}
+
+variable "dns_domain" {
+  description = "DNS domain suffix"
+  default     = "hashidemos.io"
+}
+
+variable "network_address_space" {
+  description = "CIDR for this deployment"
+  default     = "192.168.0.0/16"
+}
+
