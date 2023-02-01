b5d89e92f4989f690c35bf6eb8195ec2dfafc5fd
joestack <joern@joern-online.de>
2023-02-01 15:46:06 +0100

    dyn creds 2

--- output.tf
+++ output.tf
@@ -3,11 +3,11 @@ output "Bastionhost_public_IP" {
 }
 
 output "Bastionhost_DNS" {
-  value = aws_route53_record.bastionhost.name
+  value = aws_route53_record.bastionhost.*.name
 }
 
 output "ELB_public_DNS" {
-  value = "${aws_route53_record.elb.name}.${var.dns_domain}"
+  value = "${aws_route53_record.elb.*.name}.${var.dns_domain}"
 }
 
 # output "inventory" {
